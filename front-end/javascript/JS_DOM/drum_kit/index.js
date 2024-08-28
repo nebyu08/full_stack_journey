efde2a4997d1505182b239c23a958e2e813d4b3d
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-08-28 23:47:13 +0300

    finished the project

--- front-end/javascript/JS_DOM/drum_kit/index.js
+++ front-end/javascript/JS_DOM/drum_kit/index.js
@@ -1,8 +1,64 @@
+function makesound(check){
+    switch(check){
+        case 'w':
+            var audio=new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        
+        case 'a':
+            var audio=new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        
+        case 's':
+            var audio=new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+        case 'd':
+            var audio=new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+
+        case 'j':
+            var audio=new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case 'k':
+            var audio=new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case 'l':
+            var audio=new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+        default:
+            console.log(element)
+}
+
+}
+
+
 for(var i=0;i<document.querySelectorAll('.drum').length;i++){
     document.querySelectorAll('.drum')[i].addEventListener("click",function (){
-        this.style.color="purple";
+        var element=this.innerHTML;
+        makesound(element);
+        buttonAnimation(element);
     })
 }
 
-// var audio=new Audio('sounds/tom-1.mp3');
-//         audio.play(); 
+//event listener in js
+document.addEventListener('keypress',function(event){
+    var element=event.key;
+    makesound(element);
+    buttonAnimation(element);
+})
+
+
+function buttonAnimation(key){
+    var button=document.querySelector('.'+key);
+    button.classList.add('pressed');
+    //lets turn off the animation after some time
+    setTimeout(function(){
+        button.classList.remove('pressed');
+    },100);
+}

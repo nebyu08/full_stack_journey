0956a328f407f5ab1ca42896b940e3d731236428
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-08-28 18:00:02 +0300

    added background image and other features

--- front-end/javascript/JS_DOM/drum_kit/index.js
+++ front-end/javascript/JS_DOM/drum_kit/index.js
@@ -1,5 +1,8 @@
 for(var i=0;i<document.querySelectorAll('.drum').length;i++){
     document.querySelectorAll('.drum')[i].addEventListener("click",function (){
-        alert("i got clicked");
+        this.style.color="purple";
     })
 }
+
+// var audio=new Audio('sounds/tom-1.mp3');
+//         audio.play(); 

f1498e2c3c7661f38adc84096aee3618b45aa790
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-08-29 10:33:29 +0300

    adding class,chaning attr

--- front-end/javascript/jquery/first_exp/index.js
+++ front-end/javascript/jquery/first_exp/index.js
@@ -4,4 +4,9 @@ $('h1').addClass('big-class margin-0');
 
 $('h1').text("DanDaDaN");
 
-$('button').html('<em>Doom</em>')
+$('button').html('<em>Doom</em>');
+
+//console.log($('a').attr("href"));
+$('a').attr("href","https://www.x.com");
+
+console.log($('a').attr("href"));

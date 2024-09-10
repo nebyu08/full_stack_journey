1bbf87a3d0855d55c3db6039a1a9b3e927fbcabc
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-09-10 10:47:56 +0300

    progress

---
+++ back-end/beginer_exp/express_/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+const app=express();
+const port=3000;
+
+//root
+app.get("/",(req,res)=>{
+   res.send("<h1>Welcome Sir</h1>");
+});
+
+//about
+app.get("/about",(req,res)=>{
+    res.send("<h1>About Us</h1>")
+});
+
+app.get("/contact",(req,res)=>{
+    res.send("<p>this is the coolest paragraph ever</p>");
+});
+
+//listen port
+app.listen(port,()=>{
+    console.log(`server runnning on ${port}`);
+});

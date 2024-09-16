73ea6854fe1a32abeb2791e59fbd807c962c82c6
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-09-16 11:38:32 +0300

    good version of nav and main

--- back-end/beginer_exp/blog post/index.js
+++ back-end/beginer_exp/blog post/index.js
@@ -1,12 +1,21 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 const app=express();
 const port=3000;
+
 app.use(express.static('public'));
+app.set('views',path.join(__dirname,'views/pages'));
+
+app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
     
-    res.render('index.ejs');
+    res.render('index');
 })
 
 app.listen(port,()=>{

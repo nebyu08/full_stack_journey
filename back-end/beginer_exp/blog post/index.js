9e23cbc8f479fa82d47fe2c7b8aaf45edd097074
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-09-16 09:07:13 +0300

    added ejs template

--- back-end/beginer_exp/blog post/index.js
+++ back-end/beginer_exp/blog post/index.js
@@ -1,19 +1,12 @@
 import express from 'express';
 
-
 const app=express();
 const port=3000;
-
 app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
-    const images=[
-        "img/StockCake-Baking Fresh Bread_1726300843.jpg",
-        "img/StockCake-Cranberry Cooking Pot_1726300952.jpg",
-        "img/StockCake-Futuristic Helmet Design_1726300923.jpg"   
-    ];
-
-    res.render('index.ejs',{images});
+    
+    res.render('index.ejs');
 })
 
 app.listen(port,()=>{

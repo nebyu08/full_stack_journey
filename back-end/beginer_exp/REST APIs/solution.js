3ac3616ab69537f55ba105f6a6dd58ffd76b7b84
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-09-18 23:20:53 +0300

    add more RESTFUL ops

--- back-end/beginer_exp/REST APIs/solution.js
+++ back-end/beginer_exp/REST APIs/solution.js
@@ -7,7 +7,7 @@ const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 //Add your own bearer token from the previous lesson.
-const yourBearerToken = "08f3026d-9c6c-4d88-a3b2-c579dc106247";
+const yourBearerToken = "d47cbadf-f7ff-4d5b-aba2-09b4128056dc";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };

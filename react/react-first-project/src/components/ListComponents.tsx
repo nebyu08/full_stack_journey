fe0fc74426751851a75e1786e3ae923760d76bff
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-10-29 16:50:32 +0300

    add event handler

--- react/react-first-project/src/components/ListComponents.tsx
+++ react/react-first-project/src/components/ListComponents.tsx
@@ -1,17 +1,28 @@
+import { MouseEvent } from "react";
 
-function ListGroup(){
-    return (
-        <>
-            <h1>List</h1>
-            <ul className="list-group">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
-            </ul>
-        </>
-    )
+function ListGroup() {
+  let items = ["New York", "London", "New Jearsy", "Tokyo","Cape town" ];
+
+  //Event handler
+  const handleClick= (event:MouseEvent) => console.log(event);
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No Element</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li
+            className="list-group-item"
+            onClick={handleClick} 
+            key={item}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;

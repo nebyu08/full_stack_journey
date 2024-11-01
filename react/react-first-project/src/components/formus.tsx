0fe868b39a3ab41866bd3980c2967ef72fafc335
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-11-01 18:21:58 +0300

    use ref hook

--- react/react-first-project/src/components/formus.tsx
+++ react/react-first-project/src/components/formus.tsx
@@ -1,22 +1,41 @@
+import { FormEvent, useRef } from "react";
+
 function Form() {
-  return (
-    <form>
-      <div className="mb-3" >
-        <label htmlFor="name" className="form-Label">
-          Name
-        </label>
-        <input id="name" type="text" className="form-control" />
-      </div>
-      
-      <div className="mb-3" >
-        <label htmlFor="age" className="form-label" >age</label>
-        <input id='age' type="number" className="form-control" />
-      </div>
+    const refName=useRef<HTMLInputElement>(null);
+    const refAge=useRef<HTMLInputElement>(null);
+    const Person={name:'',age:0};
 
-      <button className="btn btn-primary" type="submit">Submit</button>
 
-    </form>
-  );
+    const handleEvent = (event: FormEvent) => {
+        event.preventDefault();
+        if(refName.current!==null)
+            Person.name=refName.current.value
+        if(refAge.current!==null)
+            Person.age=parseInt(refAge.current.value);
+
+        console.log(Person);
+    };
+    return (
+        <form onSubmit={handleEvent}>
+        <div className="mb-3">
+            <label htmlFor="name" className="form-Label">
+            Name
+            </label>
+            <input ref={refName} id="name" type="text" className="form-control" />
+        </div>
+
+        <div className="mb-3">
+            <label htmlFor="age" className="form-label">
+            age
+            </label>
+            <input ref={refAge} id="age" type="number" className="form-control" />
+        </div>
+
+        <button className="btn btn-primary" type="submit">
+            Submit
+        </button>
+        </form>
+    );
 }
 
 export default Form;

d0eee609725a06ea1869579750bfdb8d6010d47d
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-11-01 18:58:06 +0300

    add use state

--- react/react-first-project/src/components/formus.tsx
+++ react/react-first-project/src/components/formus.tsx
@@ -1,40 +1,38 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useRef, useState } from "react";
 
 function Form() {
-    const refName=useRef<HTMLInputElement>(null);
-    const refAge=useRef<HTMLInputElement>(null);
-    const Person={name:'',age:0};
+    const [person,setPerson]=useState({
+        name:'',
+        age:''
+    });
 
+    const handleEvent=(event:FormEvent)=>{
+        event.preventDefault()
+        console.log(person);
+    }
 
-    const handleEvent = (event: FormEvent) => {
-        event.preventDefault();
-        if(refName.current!==null)
-            Person.name=refName.current.value
-        if(refAge.current!==null)
-            Person.age=parseInt(refAge.current.value);
-
-        console.log(Person);
-    };
     return (
+        <>
         <form onSubmit={handleEvent}>
         <div className="mb-3">
             <label htmlFor="name" className="form-Label">
             Name
             </label>
-            <input ref={refName} id="name" type="text" className="form-control" />
+            <input value={person.name} onChange={(event)=> { setPerson({...person,name:event.target.value}) } }  id="name" type="text" className="form-control" />
         </div>
 
         <div className="mb-3">
             <label htmlFor="age" className="form-label">
             age
             </label>
-            <input ref={refAge} id="age" type="number" className="form-control" />
+            <input value={person.age} onChange={(event)=> { setPerson({...person,age:parseInt(event.target.value)}) } } id="age" type="number" className="form-control" />
         </div>
 
         <button className="btn btn-primary" type="submit">
             Submit
         </button>
         </form>
+        </>
     );
 }
 

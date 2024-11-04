bde55183f694b2fac9a957c73084b0d2d2d6c9bb
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-11-04 13:22:34 +0300

    add expensefilter component

--- react/react-first-project/src/App.tsx
+++ react/react-first-project/src/App.tsx
@@ -1,20 +1,28 @@
 import { useState } from "react";
-import ExpenseList from "./components/expensetracker/expenseList";
+import ExpenseList from "./components/expensetracker/ExpenseList";
+import ExpenseFilter from "./components/expensetracker/ExpenseFilter";
 
-function App(){
-
-  const [expenses,setExpense]=useState([
-    {id:1,discription:'aa',amount:12,category:'utilities' },
-    {id:2,discription:'aa1',amount:22,category:'utilities' },
-    {id:3,discription:'aa2',amount:45,category:'utilities' },
-    {id:4,discription:'aa3',amount:54,category:'utilities' },
-    {id:5,discription:'aa4',amount:56,category:'utilities' }
- ] )
+function App() {
+  const [expenses, setExpense] = useState([
+    { id: 1, description: "aa", amount: 12, category: "utilities" },
+    { id: 2, description: "aa1", amount: 22, category: "utilities" },
+    { id: 3, description: "aa2", amount: 45, category: "utilities" },
+    { id: 4, description: "aa3", amount: 54, category: "utilities" },
+    { id: 5, description: "aa4", amount: 56, category: "utilities" },
+  ]);
 
   return (
-   <ExpenseList expenses={expenses} onDelete={(id)=>setExpense(expenses.filter( (e) => e.id!==id )) } />
-  )
-  
+    <>
+      <div className="mb-3">
+        <ExpenseFilter onSelectCategory={ category => console.log(category)} />
+      </div>
+
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpense(expenses.filter((e) => e.id !== id))}
+      />
+    </>
+  );
 }
 
 export default App;

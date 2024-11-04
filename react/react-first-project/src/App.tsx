f8a1ff5e370669cb1e14353d3ec34a4ba7dfb7da
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-11-04 10:59:13 +0300

    initial expense list

--- react/react-first-project/src/App.tsx
+++ react/react-first-project/src/App.tsx
@@ -1,12 +1,18 @@
-import Form from "./components/formus";
-
+import { useState } from "react";
+import ExpenseList from "./components/expensetracker/expenseList";
 
 function App(){
 
+  const [expenses,setExpense]=useState([
+    {id:1,discription:'aa',amount:12,category:'utilities' },
+    {id:2,discription:'aa1',amount:22,category:'utilities' },
+    {id:3,discription:'aa2',amount:45,category:'utilities' },
+    {id:4,discription:'aa3',amount:54,category:'utilities' },
+    {id:5,discription:'aa4',amount:56,category:'utilities' }
+ ] )
+
   return (
-   <div>
-    <Form/>
-   </div>    
+   <ExpenseList expenses={expenses} onDelete={(id)=>setExpense(expenses.filter( (e) => e.id!==id )) } />
   )
   
 }
--- react/react-first-project/src/components/formus.tsx
+++ react/react-first-project/src/components/formus.tsx
@@ -1,7 +1,6 @@
 import { FieldValues, useForm} from "react-hook-form";
 import {z} from 'zod';
 import { zodResolver } from "@hookform/resolvers/zod";
-import { invalidData } from "@hookform/resolvers/ajv/src/__tests__/__fixtures__/data.js";
 
 const schema=z.object({
     name:z.string().min(3,{message:'Name must be at least 3 charachters long'}),

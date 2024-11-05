169540fc8cfe32dae4386b223ceac9d7e4113e44
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-11-05 10:50:29 +0300

    add optimistic approach deletion

--- react/react-first-project/src/App.tsx
+++ react/react-first-project/src/App.tsx
@@ -9,33 +9,49 @@ interface User {
 function App() {
   const [users, setUser] = useState<User[]>([]);
   const [error, setError] = useState("");
-  const [isLoading,setLoading]=useState(true);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    const controller= new AbortController();
+    const controller = new AbortController();
 
-    axios.get('https://jsonplaceholder.typicode.com/users',{signal:controller.signal})
-    .then((resp)=>{
-      setUser(resp.data);
-      setLoading(false);
+    axios
+      .get("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((resp) => {
+        setUser(resp.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  const deleteUSer=(user:User)=>{
+    const originalData=[...users];
+    setUser(users.filter((u)=> u.id!==user.id ));
+    // update the server
+    axios.delete("https://jsonplaceholder.typicode.com/xusers/"+user.id)
+    .catch(e=>{
+      setError(e.message);
+      setUser(originalData);
     })
-    .catch((err)=>{
-      if(err instanceof CanceledError) return ;
-      setError(err);
-      setLoading(false);
-    }  )
-
-    return ()=>controller.abort();
-
-  },[]);
+  }
 
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
-      {isLoading && <div className="spinner-border"></div> }
-      <ul>
+      {isLoading && <div className="spinner-border"></div>}
+      <ul className="list-group" >
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li key={user.id} className="list-group-item d-flex justify-content-between " >
+            {user.name}{" "}
+            <button onClick={()=> deleteUSer(user) } className="btn btn-outline-danger">Delete</button>{" "}
+          </li>
         ))}
       </ul>
     </>

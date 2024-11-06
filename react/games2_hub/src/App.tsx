ad8772e2439706f46bee0ad9d1ec6b91bb261223
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-11-06 19:14:11 +0300

    add a nav bar

--- react/games2_hub/src/App.tsx
+++ react/games2_hub/src/App.tsx
@@ -1,21 +1,27 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react"
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NabBar from "./components/NavBar";
 
 function App() {
-
   return (
-    <Grid templateAreas={{
-      base:`"nav" "main" `,
-      lg:`"nav nav " "aside main" `
-    }}>
-
-      <GridItem area="nav" bg="coral" >nav</GridItem>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main" `,
+        lg: `"nav nav " "aside main" `,
+      }}
+    >
+      <GridItem area="nav">
+        <NabBar/>
+      </GridItem>
       <Show above="lg">
-      <GridItem area="aside" bg="gold" >Aside</GridItem>
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
       </Show>
-      <GridItem area="main" bg="dodgerblue" >Main</GridItem>
-
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
     </Grid>
-  )
+  );
 }
 
-export default App
+export default App;

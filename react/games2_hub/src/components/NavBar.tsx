82b67594570e305f0ff4306d72db581c63ff71f1
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-11-06 19:56:03 +0300

    add navbar component

--- react/games2_hub/src/components/NavBar.tsx
+++ react/games2_hub/src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import { HStack, Image, Text } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
+import ColorModeSwitch from './ColorModeSwitch';
 
 const NabBar = () => {
   return (
-   <HStack>
+   <HStack padding="10px" justifyContent="space-between"  >
     <Image src={logo} boxSize='60px' />
-    <Text>NavBar</Text>
+    <ColorModeSwitch/>
    </HStack>
   )
 }

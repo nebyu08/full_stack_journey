9cb6cb6bfe4a462552e43fe592da01136cab34ee
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-11-07 19:07:31 +0300

    crop our image

--- react/games2_hub/src/components/GameCard.tsx
+++ react/games2_hub/src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames"
 import PlatformIcon from "./PlatformIcon";
 import CriticScore from "./CriticScore";
+import getCropedImageUrl from "../services/img-url";
 
 interface Props{
     game:Game;
@@ -11,7 +12,7 @@ function GameCard({game}:Props) {
 //   console.log(game.);
   return (
     <Card borderRadius={10} overflow={'hidden'} padding={10} >
-        <Image src={game.background_image} />
+        <Image src={ getCropedImageUrl(game.background_image)} />
         <CardBody>
             <Heading fontSize={'3xl'}>{game.name} </Heading>
             <HStack justifyContent='space-between' >

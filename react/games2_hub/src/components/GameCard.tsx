79c76bb9785c5af5ebae5af4a86856743c311ac8
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-11-07 17:09:28 +0300

    add icons component

--- react/games2_hub/src/components/GameCard.tsx
+++ react/games2_hub/src/components/GameCard.tsx
@@ -1,16 +1,19 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames"
+import PlatformIcon from "./PlatformIcon";
 
 interface Props{
     game:Game;
 }
 
 function GameCard({game}:Props) {
+//   console.log(game.);
   return (
     <Card borderRadius={10} overflow={'hidden'} padding={10} >
         <Image src={game.background_image} />
         <CardBody>
             <Heading fontSize={'3xl'}>{game.name} </Heading>
+            <PlatformIcon platforms={game.parent_platforms.map(p => p.platform ) } />
         </CardBody>
     </Card>
   )

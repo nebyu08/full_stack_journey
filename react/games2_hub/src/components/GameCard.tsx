37e802733a657e1da46e3cee5bac5a6b74a60708
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-11-07 18:54:49 +0300

    add critic score

--- react/games2_hub/src/components/GameCard.tsx
+++ react/games2_hub/src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames"
 import PlatformIcon from "./PlatformIcon";
+import CriticScore from "./CriticScore";
 
 interface Props{
     game:Game;
@@ -13,7 +14,10 @@ function GameCard({game}:Props) {
         <Image src={game.background_image} />
         <CardBody>
             <Heading fontSize={'3xl'}>{game.name} </Heading>
-            <PlatformIcon platforms={game.parent_platforms.map(p => p.platform ) } />
+            <HStack justifyContent='space-between' >
+                <PlatformIcon platforms={game.parent_platforms.map(p => p.platform ) } />
+                <CriticScore score={game.metacritic} />
+            </HStack>
         </CardBody>
     </Card>
   )

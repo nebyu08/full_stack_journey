a27fa2318ed544300b5a93bf1c150f93946f6928
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-11-11 17:07:01 +0300

    refactor game grid

--- react/games2_hub/src/components/GameGrid.tsx
+++ react/games2_hub/src/components/GameGrid.tsx
@@ -17,9 +17,9 @@ function GameGrid({gameQuery}:Props) {
     skeletons.push(i);
   }
 
+  if(error) return <Text>{error} </Text>
+
   return (
-    <>
-      {error && <Text>{error} </Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding={10}
@@ -37,7 +37,6 @@ function GameGrid({gameQuery}:Props) {
           </GameCardContainer>
         ))}
       </SimpleGrid>
-    </>
   );
 }
 

4e2f5a10725613ca39518fd660d229da1f059863
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-11-11 07:53:54 +0300

    highlight selected genre

--- react/games2_hub/src/components/GenreList.tsx
+++ react/games2_hub/src/components/GenreList.tsx
@@ -11,9 +11,10 @@ import getCropedImageUrl from "../services/img-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -30,6 +31,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
               src={getCropedImageUrl(genre.image_background)}
             />
             <Button
+              fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
               onClick={() => {
                 onSelectGenre(genre);
               }}

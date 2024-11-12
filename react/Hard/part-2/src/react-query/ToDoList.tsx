76e55f0c8a339d1db9a9ea7f295646ffde5a7884
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-11-12 10:27:48 +0300

    initial commit for part 2

---
+++ react/Hard/part-2/src/react-query/ToDoList.tsx
@@ -0,0 +1,38 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+interface Todo {
+  id: number;
+  title: string;
+  userId: number;
+  completed: boolean;
+}
+
+const TodoList = () => {
+
+  const fetchToDos=()=>
+    axios
+    .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
+    .then(res => res.data)
+  
+
+  const {data:todos,error}= useQuery<Todo[],Error>({
+      queryKey:['todos'],
+      queryFn:fetchToDos
+  });
+
+
+  if (error) return <p>{error.message}</p>;
+
+  return (
+    <ul className="list-group">
+      {todos?.map((todo) => (
+        <li key={todo.id} className="list-group-item">
+          {todo.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;

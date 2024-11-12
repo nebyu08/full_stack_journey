62448cb10046d06235923b82b20a571accac427d
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-11-12 11:54:48 +0300

    add post component

---
+++ react/Hard/part-2/src/react-query/hooks/usePost.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+    userId: number;
+}
+
+const usePosts=()=>{
+    const fetchPosts=()=>
+        axios
+        .get<Post[]>('https://jsonplaceholder.typicode.com/todos')
+        .then(res => res.data)
+    
+    return  useQuery<Post[],Error>({
+        queryKey:['posts'],
+        queryFn:fetchPosts,
+        staleTime:1*60*1000 //1 min
+    });
+
+}
+
+export default usePosts;
+

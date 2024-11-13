fad2b3f3d4b3b04170f13bbff66dce20c2735318
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-11-13 12:14:22 +0300

    mutating data

---
+++ react/Hard/part-2/src/react-query/TodoForm.tsx
@@ -0,0 +1,50 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRef } from "react";
+import { Todo } from "./hooks/useTodos";
+import axios from "axios";
+
+const TodoForm = () => {
+  const ref = useRef<HTMLInputElement>(null);
+  const queryClient= useQueryClient();
+  const addTodo = useMutation({
+    mutationFn: (todo: Todo) => 
+      axios
+        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
+        .then((res) => res.data),
+
+    onSuccess:(savedTodo,newTodo)=>{
+        // console.log(savedTodo)
+        // queryClient.invalidateQueries({
+        //     queryKey:['todos']
+        // })
+         
+        // updating the cache
+        queryClient.setQueryData<Todo[]>(['todos'],todos => [savedTodo,...(todos || [] )  ] )
+    }
+    });
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if(ref.current && ref.current.value)
+        addTodo.mutate({
+            id:0,
+            title:ref.current?.value,
+            completed:false,
+            userId:1
+        })
+      }}
+      className="row mb-3"
+    >
+      <div className="col">
+        <input ref={ref} type="text" className="form-control" />
+      </div>
+      <div className="col">
+        <button className="btn btn-primary">Add</button>
+      </div>
+    </form>
+  );
+};
+
+export default TodoForm;
--- react/Hard/part-2/src/react-query/hooks/usePost.ts
+++ react/Hard/part-2/src/react-query/hooks/usePost.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
-interface Post {
+export interface Post {
     id: number;
     title: string;
     body: string;
@@ -9,29 +9,26 @@ interface Post {
 }
 
 interface PostQuery{
-    page:number;
     pageSize:number
 }
 
-const usePosts=(query:PostQuery)=>{
-        
-    return  useQuery<Post[],Error>({
-        queryKey:['posts',query] ,
-        queryFn:()=>
-            axios
-            .get<Post[]>('https://jsonplaceholder.typicode.com/todos',{
-                params:{
-                    _start:(query.page -1) *query.pageSize,
-                    _limit:query.pageSize
+const usePosts=(query:PostQuery)=> useInfiniteQuery<Post[], Error>({
+    queryKey: ['posts', query],
+    queryFn: ({ pageParam = 1 }) => 
+        axios
+            .get<Post[]>('https://jsonplaceholder.typicode.com/todos', {
+                params: {
+                    _start: (pageParam - 1) * query.pageSize,
+                    _limit: query.pageSize
                 }
             })
             .then(res => res.data),
 
-        staleTime:1*60*1000, //1 min
-        placeholderData:(prevData)=> prevData || []
-    });
-
-};
+    staleTime: 1 * 60 * 1000, //1 min
+    getNextPageParam: (lastPage, allPages) => {
+        return lastPage.length > 0 ? allPages.length + 1 : undefined;
+    }
+});
 
 export default usePosts;
 

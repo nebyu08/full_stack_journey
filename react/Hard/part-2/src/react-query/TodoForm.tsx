22d888157ba8d0b039f793202af36ac65c547857
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-11-13 16:13:14 +0300

    add useaddtodos and constants

--- react/Hard/part-2/src/react-query/TodoForm.tsx
+++ react/Hard/part-2/src/react-query/TodoForm.tsx
@@ -1,55 +1,18 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRef } from "react";
-import { Todo } from "./hooks/useTodos";
-import axios from "axios";
-
-interface AddTodoContext{
-    previousTodos:Todo[];
-}
+import useAddTodo from "./hooks/useAddTodo";
 
 const TodoForm = () => {
   const ref = useRef<HTMLInputElement>(null);
-  const queryClient = useQueryClient();
-  const addTodo = useMutation<Todo,Error,Todo,AddTodoContext> ({
-    mutationFn: (todo: Todo) =>
-      axios
-        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
-        .then((res) => res.data),
-    
-    onMutate:(newTodo:Todo)=>{
-        const previousTodos=queryClient.getQueryData<Todo[]>(['todos']) || [] ;
-
-        queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
-            newTodo,
-            ...(todos || []),
-          ]);         
-         if(ref.current) ref.current.value='';
-
-         return {previousTodos}
-    },
-
-    onSuccess:(saveTodo,newTodo) => {
-      // updating the cache
-      queryClient.setQueryData<Todo[]>(['todos'],(todos)=>
-        todos?.map((todo)=>
-            todo === newTodo ? saveTodo : todo
-        )
-    );   
-    },
-    
-    onError:(error,newTodo,context)=>{
-        if(!context) return ;
-
-        queryClient.setQueryData<Todo[]>(['todos'],context.previousTodos);
-    }
-  });
-
-
+  const addTodo =useAddTodo( () => {
+    if(ref.current) ref.current.value=''
+  } )
+  
   return (
     <>
     {addTodo.error && <div className="alert alert-danger">
         {addTodo.error.message}
      </div> }
+     
       <form
         onSubmit={(event) => {
           event.preventDefault();
